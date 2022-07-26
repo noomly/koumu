ea64c9c05d8791d81a9946c7b4080570440920e8
Eyal Chojnowski <eyal.choj@gmail.com>
2022-07-26 09:39:18 +0200

    :bug: Fix setup script for "other" pkg managers

--- src/cli/setup.ts
+++ src/cli/setup.ts
@@ -5,63 +5,34 @@ import { exhaustive } from "@/utils";
 
 declare const KOUMU_VERSION: string;
 
-function setupOther() {
-    const pkg = JSON.parse(readFileSync("./package.json").toString());
-
-    if (!pkg.devDependencies) {
-        pkg.devDependencies = {};
-    }
-    if (!pkg.scripts) {
-        pkg.scripts = {};
-    }
-
-    const { scripts } = pkg;
-    const devDeps = pkg.devDependencies;
-
-    devDeps.koumu = KOUMU_VERSION;
-    devDeps.husky = "8.0.1";
-
-    scripts.prepare = "husky install";
-
-    writeFileSync("./package.json", JSON.stringify(pkg, null, 4));
-}
-
-function setupYarn2() {
-    const pkg = JSON.parse(readFileSync("./package.json").toString());
-
-    if (!pkg.devDependencies) {
-        pkg.devDependencies = {};
-    }
-    if (!pkg.scripts) {
-        pkg.scripts = {};
-    }
-
-    const { scripts } = pkg;
-    const devDeps = pkg.devDependencies;
-
-    devDeps.koumu = KOUMU_VERSION;
-    devDeps.pinst = "3.0.0";
-    devDeps.husky = "8.0.1";
-
-    scripts.postinstall =
-        "husky install && cp ./node_modules/koumu/build/commit-msg .husky/commit-msg";
-    scripts.prepack = "pinst --disable";
-    scripts.postpack = "pinst --enable";
-
-    writeFileSync("./package.json", JSON.stringify(pkg, null, 4));
-}
-
 export default function setup(pkgManager: PkgManager) {
-    switch (pkgManager) {
-        case "yarn2":
-            setupYarn2();
-            break;
-        case "other":
-            setupOther();
-            break;
-        default:
-            exhaustive(pkgManager);
+    const pkg = JSON.parse(readFileSync("./package.json").toString());
+
+    if (!pkg.devDependencies) {
+        pkg.devDependencies = {};
     }
+    if (!pkg.scripts) {
+        pkg.scripts = {};
+    }
+
+    const { scripts } = pkg;
+    const devDeps = pkg.devDependencies;
+
+    devDeps.koumu = KOUMU_VERSION;
+    devDeps.husky = "8.0.1";
+
+    const installScript =
+        "husky install && cp ./node_modules/koumu/build/commit-msg .husky/commit-msg";
+
+    if (pkgManager === "yarn2") {
+        scripts.postinstall = installScript;
+        scripts.prepack = "pinst --disable";
+        scripts.postpack = "pinst --enable";
+    } else {
+        scripts.prepare = installScript;
+    }
+
+    writeFileSync("./package.json", JSON.stringify(pkg, null, 4));
 
     console.log(
         "Your package.json has been setup to run Koumu via Husky. Please run the install command " +
