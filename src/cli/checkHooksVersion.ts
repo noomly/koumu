4560998b193b246a8ac753b1feabef10a20129b3
Eyal Chojnowski <eyal.choj@gmail.com>
2022-10-02 17:51:17 +0200

    :sparkles: Implement detection for hooks version mismatch

---
+++ src/cli/checkHooksVersion.ts
@@ -0,0 +1,108 @@
+import { createReadStream, existsSync, statSync } from "node:fs";
+import { createInterface } from "node:readline/promises";
+import { join } from "node:path";
+
+import chalk from "chalk";
+
+import { findProjectRoot } from "@/utils";
+import { VERSION } from "@/cli/consts";
+
+function getHookVersion(path: string): Promise<string | undefined> {
+    return new Promise((resolve, _) => {
+        const rl = createInterface({
+            input: createReadStream(path),
+            crlfDelay: Infinity,
+        });
+
+        let version: string | undefined;
+        let lineCount = 0;
+
+        rl.on("line", (line) => {
+            const lineLowerCase = line.toLowerCase();
+            if (lineLowerCase.includes("koumu") && lineLowerCase.includes("version")) {
+                // eslint-disable-next-line prefer-destructuring
+                version = line.split(": ")[1];
+            }
+
+            if (lineCount >= 1) {
+                rl.close();
+            }
+
+            lineCount += 1;
+        });
+
+        rl.on("close", () => {
+            resolve(version);
+        });
+    });
+}
+
+async function shouldRunSetupFor(dir: string, hooks: string[]): Promise<boolean> {
+    for (const hook of hooks) {
+        const hookPath = join(dir, hook);
+
+        if (!existsSync(hookPath) || !statSync(hookPath).isFile()) {
+            return false;
+        }
+
+        const version = await getHookVersion(hookPath);
+
+        if (!version) {
+            return false;
+        }
+
+        if (VERSION !== version) {
+            console.log(
+                chalk.blue(
+                    `At least one hook version doesn't match Koumu's in ${chalk.gray(dir)}` +
+                        ` (hook ${chalk.grey(version)} != koumu ${chalk.cyan(VERSION)}).`,
+                ),
+            );
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export async function checkHooksVersion() {
+    const projectRoot = findProjectRoot();
+
+    if (!projectRoot) {
+        return;
+    }
+
+    const huskyDir = join(projectRoot, ".husky");
+    const gitDir = join(projectRoot, ".git", "hooks");
+
+    const hooks = ["commit-msg", "prepare-commit-msg"];
+
+    let shouldSetup: "husky" | "git" | undefined;
+
+    if (
+        existsSync(huskyDir) &&
+        statSync(huskyDir).isDirectory() &&
+        (await shouldRunSetupFor(huskyDir, hooks))
+    ) {
+        shouldSetup = "husky";
+    }
+
+    if (
+        !shouldSetup &&
+        existsSync(gitDir) &&
+        statSync(gitDir).isDirectory() &&
+        (await shouldRunSetupFor(gitDir, hooks))
+    ) {
+        shouldSetup = "git";
+    }
+
+    if (shouldSetup) {
+        console.log(
+            chalk.blue(
+                "Run " +
+                    chalk.cyan(`koumu setup --${shouldSetup}`) +
+                    ` to sync the hooks to ${chalk.cyan(VERSION)}.\n`,
+            ),
+        );
+    }
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -4,53 +4,58 @@ import { VERSION, COMMIT_MSG_BUILD, PREPARE_COMMIT_MSG_BUILD, DEFAULT_CONFIG } f
 import commit from "@/cli/commit";
 import setup, { SetupMode, SETUP_MODES } from "@/cli/setup";
 import { writeConfig } from "@/cli/writeConfig";
+import { checkHooksVersion } from "@/cli/checkHooksVersion";
 
-const program = new Command().name("koumu").version(VERSION);
+checkHooksVersion().then(runCli);
 
-program
-    .command("write-config <path>")
-    .description(`write the default config to <path>`)
-    .action((options) => {
-        writeConfig(DEFAULT_CONFIG, options);
-    });
+function runCli() {
+    const program = new Command().name("koumu").version(VERSION);
 
-program
-    .command("setup")
-    .option("--npm", "setup Koumu for NPM or Yarn v1")
-    .option("--yarn", "alias for --npm")
-    .option("--yarn2", "setup Koumu for Yarn v2+")
-    .option(
-        "--copy-into-husky, --husky",
-        "copy Koumu's hooks into the .husky directory (use this if you've already setup Husky)",
-    )
-    .option(
-        "--copy-into-git, --git",
-        "only install Koumu's hooks into standard git directory (use this if" +
-            " your repo doesn't host a JS project)",
-    )
-    .action((rawOptions: Record<SetupMode, boolean>, cmd) => {
-        const options = Object.keys(rawOptions) as SetupMode[];
-        if (options.length !== 1 || !SETUP_MODES.includes(options[0])) {
-            cmd.help();
-        }
+    program
+        .command("write-config <path>")
+        .description(`write the default config to <path>`)
+        .action((options) => {
+            writeConfig(DEFAULT_CONFIG, options);
+        });
 
-        setup(options[0], COMMIT_MSG_BUILD, PREPARE_COMMIT_MSG_BUILD);
-    });
+    program
+        .command("setup")
+        .option("--npm", "setup Koumu for NPM or Yarn v1")
+        .option("--yarn", "alias for --npm")
+        .option("--yarn2", "setup Koumu for Yarn v2+")
+        .option(
+            "--copy-into-husky, --husky",
+            "copy Koumu's hooks into the .husky directory (use this if you've already setup Husky)",
+        )
+        .option(
+            "--copy-into-git, --git",
+            "only install Koumu's hooks into standard git directory (use this if" +
+                " your repo doesn't host a JS project)",
+        )
+        .action((rawOptions: Record<SetupMode, boolean>, cmd) => {
+            const options = Object.keys(rawOptions) as SetupMode[];
+            if (options.length !== 1 || !SETUP_MODES.includes(options[0])) {
+                cmd.help();
+            }
 
-program
-    .command("commit")
-    .option("-e --external", "use an external editor to edit the commit message")
-    .option(
-        "-i --issue",
-        "(requires github cli) interactively select an issue that will be added to the footer",
-    )
-    .option(
-        "-c --closes-issue",
-        "(requires github cli) interactively select an issue that will be added to the footer," +
-            ' prepended with "closes"',
-    )
-    .action((options: Record<"external" | "issue" | "closesIssue", boolean>, _cmd) => {
-        commit(options.external, options.issue, options.closesIssue);
-    });
+            setup(options[0], COMMIT_MSG_BUILD, PREPARE_COMMIT_MSG_BUILD);
+        });
 
-program.parse();
+    program
+        .command("commit")
+        .option("-e --external", "use an external editor to edit the commit message")
+        .option(
+            "-i --issue",
+            "(requires github cli) interactively select an issue that will be added to the footer",
+        )
+        .option(
+            "-c --closes-issue",
+            "(requires github cli) interactively select an issue that will be added to the footer," +
+                ' prepended with "closes"',
+        )
+        .action((options: Record<"external" | "issue" | "closesIssue", boolean>, _cmd) => {
+            commit(options.external, options.issue, options.closesIssue);
+        });
+
+    program.parse();
+}
